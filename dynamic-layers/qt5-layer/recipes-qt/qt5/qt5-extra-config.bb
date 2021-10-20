dh-electronics/meta-dhsom-extras

commit 6d5dc02df45a600758513ac5f14453e6c4cafcbf

dynamic/qt5-layer: qtbase: Split out qt5 configuration into qt5-extra-config
Avoid making qtbase board specific, pull qt5 configuration extras into
separate qt5-extra-config recipe and install this new recipe into demo
image. No functional change.

Signed-off-by: Marek Vasut <marex@denx.de>

dynamic-layers/qt5-layer/recipes-qt/qt5/qt5-extra-config.bb
@@ -1,0 +1,27 @@
+DESCRIPTION = "Extra configuration files for qt5"
+LICENSE = "LGPLv2.1"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/LGPL-2.1;md5=1a6d268fd218675ffea8be556788b780"
+
+inherit allarch
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+EXCLUDE_FROM_SHLIBS = "1"
+INHIBIT_DEFAULT_DEPS = "1"
+INHIBIT_PACKAGE_DEBUG_SPLIT = "1"
+INHIBIT_PACKAGE_STRIP = "1"
+
+S = "${WORKDIR}"
+
+SRC_URI:append = " \
+	file://qt5-eglfs-kms.json \
+	file://qt5-eglfs.sh \
+	file://LICENSE \
+	"
+
+do_install () {
+	install -d ${D}${sysconfdir}/default/
+	install -m 0644 ${WORKDIR}/qt5-eglfs-kms.json ${D}${sysconfdir}/default/
+
+	install -d ${D}${sysconfdir}/profile.d/
+	install -m 0755 ${WORKDIR}/qt5-eglfs.sh ${D}${sysconfdir}/profile.d/
+}

dynamic-layers/qt5-layer/recipes-core/images/dh-image-demo.bbappend
@@ -1,6 +1,7 @@
 inherit populate_sdk_qt5
 
 IMAGE_INSTALL:append:dh-dhsom = " \
+	qt5-extra-config \
 	qtwebengine-minimal qtwebengine-examples qtbase-examples \
 	qtmultimedia-examples qtwayland \
 	"

dynamic-layers/qt5-layer/recipes-qt/qt5/qtbase_%.bbappend
@@ -1,19 +1,4 @@
-FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
-
 PACKAGECONFIG:append:dh-dhsom = " \
 	examples eglfs linuxfb gles2 kms gbm fontconfig freetype accessibility \
 	xkbcommon libinput \
 	"
-
-SRC_URI:append:dh-dhsom = " \
-	file://qt5-eglfs-kms.json \
-	file://qt5-eglfs.sh \
-	"
-
-do_install:append:dh-dhsom() {
-	install -d ${D}${sysconfdir}/default/
-	install -m 0644 ${WORKDIR}/qt5-eglfs-kms.json ${D}${sysconfdir}/default/
-
-	install -d ${D}${sysconfdir}/profile.d/
-	install -m 0755 ${WORKDIR}/qt5-eglfs.sh ${D}${sysconfdir}/profile.d/
-}

commit 2efcb552125b316f6c59ec79900554e761a9f460

dynamic/qt5-layer: Drop bogus LICENSE file from SRC_URI
The recipe uses standard LGPL-2.1 license text from OE-core,
the LICENSE file in SRC_URI is not present in the layer and
would be duplicate if it were, so drop it from the SRC_URI.

Fixes: 6d5dc02 ("dynamic/qt5-layer: qtbase: Split out qt5 configuration into qt5-extra-config")
Reported-by: Martina Schmoeller <mschmoeller@dh-electronics.com>
Signed-off-by: Marek Vasut <marex@denx.de>

dynamic-layers/qt5-layer/recipes-qt/qt5/qt5-extra-config.bb
@@ -15,7 +15,6 @@ S = "${WORKDIR}"
 SRC_URI:append = " \
 	file://qt5-eglfs-kms.json \
 	file://qt5-eglfs.sh \
-	file://LICENSE \
 	"
 
 do_install () {